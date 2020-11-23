11461ef10116a047823e27c3ec45d57ecf6fbc03
Steve Dougherty <steve@asksteved.com>
2020-11-22 19:42:23 -0500

    Add initial vote receipt support

--- config_sample.py
+++ config_sample.py
@@ -1,2 +1,9 @@
+# Used to list candidates.
 BLT_PATH = "example-easier.blt"
+
+# Used to keep running ballot totals.
 PICKLE_PATH = "example-easier.pickle"
+
+# Change this to a long random string of bytes.
+# See https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
+SECRET_KEY = b'\xcd&\x91\x18D\x99\xc8\x83\x1a\xf5\xa1\xf8\x86\x8f\xf98\xc2\xd2O7,\xdb\x9ag\xba(\xb3\x1c\t\xc0\xfeB'
--- webui/app.py
+++ webui/app.py
@@ -1,5 +1,5 @@
 from typing import List
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, flash
 from format.blt import load_blt, BLT, Ballot
 import pickle
 import fcntl
@@ -12,7 +12,7 @@ blt: BLT = None
 
 # Lists of form element IDs, in candidate order, ordered from first to last
 # preference.
-candidate_options: List[List[str]] = list()
+candidate_options: List[List[int]] = list()
 
 
 @app.before_first_request
@@ -29,7 +29,7 @@ def initialize():
         choice_level = list()
         candidate_options.append(choice_level)
         for candidate_number in range(1, len(blt.candidate_names) + 1):
-            choice_level.append(f"choice_index:candidate_number")
+            choice_level.append(candidate_number)
 
     # Write the BLT pickle if it does not already exist
     try:
@@ -51,12 +51,29 @@ def vote_form():
 
 @app.route("/vote", methods=["POST"])
 def submit_vote():
-    # TODO: parse ballot out of radio buttons
-    ballot = Ballot()
-    with open(app.config["PICKLE_PATH"], "+b") as pickle_file:
-        fcntl.flock(pickle_file, fcntl.F_EXLCK)
+    choices = list()
+    for choice_number in range(1, len(blt.candidate_names) + 1):
+        if str(choice_number) in request.form:
+            choice = int(request.form[str(choice_number)])
+            if choice not in choices:
+                choices.append(choice)
+
+    ballot = Ballot(choices)
+    description = ', '.join(map(lambda n: blt.candidate_names[n - 1], choices))
+    flash(f"Your ballot for {description} has been received.")
+
+    with open(app.config["PICKLE_PATH"], "r+b") as pickle_file:
+        fcntl.flock(pickle_file, fcntl.LOCK_EX)
+
         running_results: BLT = pickle.load(pickle_file)
+
         running_results.add_ballot(ballot)
-        return ""
+        pickle_file.seek(0)
+        pickle.dump(running_results, pickle_file, pickle.HIGHEST_PROTOCOL)
+
+        return redirect(url_for("receipt"))
 
 
+@app.route("/receipt")
+def receipt():
+    return render_template("receipt.html")
